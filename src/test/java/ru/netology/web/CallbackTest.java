acd2b3408f301ff04de5e9fc2e281b88ef53468a
Mariia <lebedevamv1@gmail.com>
2022-05-07 10:47:23 +0300

    second change

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,12 +1,12 @@
 package ru.netology.web;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -17,14 +17,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CallbackTest {
     WebDriver driver;
 
-
-
     @BeforeAll
-    public  static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "C:\\QA\\AQA\\HW3v1\\driver\\win\\chromedriver.exe");
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
     }
+
     @BeforeEach
-    public  void setUp0() {
+    public void setUp1() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -32,22 +31,46 @@ public class CallbackTest {
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
+
     @AfterEach
-    public  void tearDown() {
+    public void close() {
         driver.quit();
         driver = null;
     }
-    @Test
-    public void test() {
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Лебедева Мария");
-        elements.get(1).sendKeys("+79060410000");
-        driver.findElement(By.className("checkbox__control")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("alert-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена!",text.trim());
 
+
+    @Test
+    public void shouldLastNameWithDash() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Лебедева-Парасюк Мария");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79060410000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
+    @Test
+    public void shouldNameAndLastNameWithDashe() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Лебедева-Парасюк Мария-Анна");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79060410000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+//        @Test
+//        public void shouldBeACheckOfWritingOnlyRussian() {
+//            driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Lebedeva Mariia");
+//            driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79060410000");
+//            driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+//            driver.findElement(By.tagName("button")).click(); // Отправить
+//
+//            String text = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText();
+//            assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+//        }
+
+
+
 }
 
